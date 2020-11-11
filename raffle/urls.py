33eda6000baa8c6705a3151c4c9d7f261b2d3edb
maxs <shepherd.max@gmail.com>
2020-11-11 16:52:11 +0000

    Add a homepage and an (incomplete) navbar

--- raffle/urls.py
+++ raffle/urls.py
@@ -19,9 +19,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from raffle.views import HomePageView
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('accounts/', include('account.urls')),
                   path('event/', include('event.urls')),
                   path('private_files/', include('private_files.urls')),
+                  path('', HomePageView.as_view(), name='home')
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
