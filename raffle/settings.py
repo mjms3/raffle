3a8cc7fb8930072d9ff76b0935f379bf52721cf2
maxs <shepherd.max@gmail.com>
2020-11-22 10:42:30 +0000

    Set DEBUG correctly

--- raffle/settings.py
+++ raffle/settings.py
@@ -24,11 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [env('ALLOWED_HOST')]
 
