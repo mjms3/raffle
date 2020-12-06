38c41e9fd9acbd6138f3c3a3327e8187795386b1
maxs <shepherd.max@gmail.com>
2020-12-06 16:58:37 +0000

    Improve admin interface including allowing actions to be triggered from there

--- event/admin.py
+++ event/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from event.models import Gift, RaffleEvent, RaffleParticipation, Action
 
-admin.site.register(Gift)
-admin.site.register(RaffleEvent)
+
+class RaffleEventAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'phase',
+        'raffle_actions',
+    )
+
+    def raffle_actions(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Change picker</a>&nbsp;'
+            '<a class="button" href="{}">Permute Images</a>',
+            reverse('change_picker', args=[obj.pk]),
+            reverse('permute_images', args=[obj.pk]),
+        )
+    raffle_actions.short_description = 'Raffle Actions'
+    raffle_actions.allow_tags = True
+
+class GiftAdmin(admin.ModelAdmin):
+    list_display = (
+        'description',
+        'added_by',
+        'wrapped'
+    )
+
+class RaffleParticipationAdmin(admin.ModelAdmin):
+    list_display = (
+        'event',
+        'user',
+        'number_of_tickets',
+        'number_of_times_drawn',
+    )
+
+admin.site.register(Gift,GiftAdmin)
+admin.site.register(RaffleEvent, RaffleEventAdmin)
 admin.site.register(RaffleParticipation)
 admin.site.register(Action)
