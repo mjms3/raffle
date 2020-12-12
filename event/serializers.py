260223d79645c1a2b57201fa6e15db5be16e180b
maxs <shepherd.max@gmail.com>
2020-12-12 11:32:55 +0000

    Allow for the fact that people may have transferred a gift but not participated in the raffle

--- event/serializers.py
+++ event/serializers.py
@@ -35,6 +35,21 @@ class GiftSerializer(ModelSerializer):
         fields = '__all__'
 
 
+def _get_empty_user_summary(user):
+    return {
+        'username': user.username,
+        'email': user.email,
+        'display_name': user.display_name,
+        'tickets_purchased': 0,
+        'times_drawn': 0,
+        'total_owed_for_tickets': 0,
+        'raffle_prizes': [],
+        'donation_final_locations': [],
+        'transfers': [],
+    }
+
+
+
 class RaffleEventSerializer(ModelSerializer):
     raffle_participations = RaffleParticipationSerializer(source='raffleparticipation_set', many=True)
     actions = ActionSerializer(source='action_set', many=True)
@@ -55,34 +70,19 @@ class RaffleEventSerializer(ModelSerializer):
         summary_data['total_tickets'] = total_tickets
         summary_data['number_of_swaps'] = len(transfers)
         summary_data['total_raised'] = 5 * (summary_data['total_tickets'] + summary_data['number_of_swaps'])
-        user_summary = {p.user.username:
-            {
-                'username': p.user.username,
-                'email': p.user.email,
-                'display_name': p.user.display_name,
+        user_summaries = {p.user.username: _get_empty_user_summary(p.user) for p in participations}
+        for p in participations:
+            user_summary = user_summaries[p.user.username]
+            user_summary.update({
                 'tickets_purchased': p.number_of_tickets,
                 'times_drawn': p.number_of_times_drawn,
                 'total_owed_for_tickets': 5 * p.number_of_tickets,
-                'raffle_prizes': [],
-                'donation_final_locations': [],
-                'transfers': [],
-            } for p in participations
-        }
+            })
         for gift in gifts:
-            user_summary[gift.given_to.username]['raffle_prizes'].append(gift.description)
-            if gift.added_by.username not in user_summary:
-                user_summary[gift.added_by.username] = {
-                    'username': gift.added_by.username,
-                    'email': gift.added_by.email,
-                    'display_name': gift.added_by.display_name,
-                    'tickets_purchased': 0,
-                    'times_drawn': 0,
-                    'total_owed_for_tickets': 0,
-                    'raffle_prizes': [],
-                    'donation_final_locations': [],
-                    'transfers': [],
-                }
-            user_summary[gift.added_by.username]['donation_final_locations'].append(
+            user_summaries[gift.given_to.username]['raffle_prizes'].append(gift.description)
+            if gift.added_by.username not in user_summaries:
+                user_summaries[gift.added_by.username] = _get_empty_user_summary(gift.added_by)
+            user_summaries[gift.added_by.username]['donation_final_locations'].append(
                 {
                     'gift': gift.description,
                     'user': {
@@ -93,10 +93,12 @@ class RaffleEventSerializer(ModelSerializer):
                 }
             )
         for transfer in transfers:
-            user_summary[transfer.by_user.username]['transfers'].append(transfer.message)
-        for v in user_summary.values():
+            if transfer.by_user.username not in user_summaries:
+                user_summaries[transfer.by_user.username] = _get_empty_user_summary(transfer.by_user)
+            user_summaries[transfer.by_user.username]['transfers'].append(transfer.message)
+        for v in user_summaries.values():
             v['total_transfers'] = len(v['transfers'])
             v['total_owed_for_transfers'] = 5*v['total_transfers']
             v['total_owed'] = v['total_owed_for_transfers']+v['total_owed_for_tickets']
-        summary_data['user_summary'] = user_summary
+        summary_data['user_summaries'] = user_summaries
         return summary_data
